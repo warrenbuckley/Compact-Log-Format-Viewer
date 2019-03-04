9e9f42684d96588437b410ee39a64a7830c32692
Warren Buckley <warren@creativewebspecialist.co.uk>
2019-03-04 20:27:05 +0000

    Seen this on OSX - not sure I am really gonna want this. We can add it back if there is a real accessability need to do so.

--- LogViewer.Client/src/main/appmenu.ts
+++ LogViewer.Client/src/main/appmenu.ts
@@ -105,18 +105,6 @@ if (process.platform === "darwin") {
     ],
   });
 
-  // Edit menu
-  template[1].submenu.push(
-    { type: "separator" },
-    {
-      label: "Speech",
-      submenu: [
-        { role: "startspeaking" },
-        { role: "stopspeaking" },
-      ],
-    },
-  );
-
   // Window menu
   template[3].submenu = [
     { role: "close" },
