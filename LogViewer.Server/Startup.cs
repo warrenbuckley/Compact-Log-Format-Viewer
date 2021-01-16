54dc5005e755938a034ca3b55b5ed7e5256c8bfe
Warren Buckley <warren@umbraco.com>
2021-01-16 17:10:43 +0000

    Avoid using Sytem.Text.Json as requires too much config to serilaize Serilog logs so lets use Newtonsoft

--- LogViewer.Server/Startup.cs
+++ LogViewer.Server/Startup.cs
@@ -16,7 +16,7 @@ namespace LogViewer.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSingleton<ILogParser, LogParser>();
         }
 
