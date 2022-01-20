561575d3878ca70efe5ea91a31fb72c6e1bdda0e
Warren Buckley <warren@umbraco.com>
2022-01-20 09:44:11 +0000

    Remove depercated npm package request and use Axios - Fixes issue with strange chars in filepath too

--- LogViewer.Client/src/main/webapi.ts
+++ LogViewer.Client/src/main/webapi.ts
@@ -1,5 +1,5 @@
 import { app, dialog, WebContents } from "electron";
-import request = require("request");
+import axios from "axios";
 import { updateMenuEnabledState } from "./appmenu";
 
 const serverApiDomain = "http://localhost:45678/api/Viewer";
@@ -10,15 +10,15 @@ export function openFile(filePath: string, focusedWindow: WebContents):void {
     focusedWindow.send("logviewer.loading", true);
     focusedWindow.send("logviewer.emptystate", false);
 
-    request(`${serverApiDomain}/Open?filePath=${filePath}`, { json: true }, (err, res, body) => {
-        if (res.statusCode === 400) {
-            focusedWindow.send("logviewer.error", body);
-            focusedWindow.send("logviewer.loading", false);
-
-            dialog.showErrorBox("Error", body);
-
-            return;
+    axios.get('/open', {
+        baseURL: serverApiDomain,
+        responseType: "json",
+        params: {
+            filePath: filePath,
         }
+    })
+    .then(function (response) {
+        // response.data - the response body
 
         updateMenuEnabledState("logviewer.open", false);
         updateMenuEnabledState("logviewer.close", true);
@@ -29,7 +29,7 @@ export function openFile(filePath: string, focusedWindow: WebContents):void {
         // Lets assume the Electron API here deals with dupes etc
         app.addRecentDocument(filePath);
 
-        focusedWindow.send("logviewer.file-opened", body);
+        focusedWindow.send("logviewer.file-opened", response.data);
         focusedWindow.send("logviewer.loading", false);
 
         // Call Further Init API Endpoints
@@ -39,7 +39,15 @@ export function openFile(filePath: string, focusedWindow: WebContents):void {
         getMessageTemplates(focusedWindow);
         getLogs(focusedWindow, 1, "");
 
-    });
+      })
+      .catch(function (error) {
+        if (error.response.status === 400) {
+            focusedWindow.send("logviewer.error", error.response.data);
+            focusedWindow.send("logviewer.loading", false);
+            dialog.showErrorBox("Error", error.response.data);
+            return;
+        }
+      });
 }
 
 export function reload(focusedWindow: WebContents):void {
@@ -47,22 +55,17 @@ export function reload(focusedWindow: WebContents):void {
     focusedWindow.send("logviewer.loading", true);
     focusedWindow.send("logviewer.emptystate", false);
 
-    request(`${serverApiDomain}/reload`, { json: true }, (err, res, body) => {
-        if (res.statusCode === 400) {
-            focusedWindow.send("logviewer.error", body);
-            focusedWindow.send("logviewer.loading", false);
-
-            dialog.showErrorBox("Error", body);
-
-            return;
-        }
-
+    axios.get('/reload', {
+        baseURL: serverApiDomain,
+        responseType: "json"
+    })
+    .then(function (response) {
         updateMenuEnabledState("logviewer.open", false);
         updateMenuEnabledState("logviewer.close", true);
         updateMenuEnabledState("logviewer.reload", true);
         updateMenuEnabledState("logviewer.export", true);
 
-        focusedWindow.send("logviewer.file-opened", body);
+        focusedWindow.send("logviewer.file-opened", response.data);
         focusedWindow.send("logviewer.loading", false);
 
         // Call Further Init API Endpoints
@@ -71,69 +74,99 @@ export function reload(focusedWindow: WebContents):void {
         getTotals(focusedWindow);
         getMessageTemplates(focusedWindow);
         getLogs(focusedWindow, 1, "");
-
-    });
+    })
+    .catch(function (error) {
+        if (error.response.status === 400) {
+            focusedWindow.send("logviewer.error", error.response.data);
+            focusedWindow.send("logviewer.loading", false);
+            dialog.showErrorBox("Error", error.response.data);
+            return;
+        }
+    });    
 }
 
 function getErrors(focusedWindow: WebContents) {
 
-    request(`${serverApiDomain}/errors`, { json: true }, (err, res, body) => {
-        if (err) {
-            focusedWindow.send("logviewer.error", err);
-            return console.log(err);
-        }
-
-        focusedWindow.send("logviewer.data-errors", body);
+    axios.get('/errors', {
+        baseURL: serverApiDomain,
+        responseType: "json"
+    })
+    .then(function (response) {
+        focusedWindow.send("logviewer.data-errors", response.data);
+    })
+    .catch(function (error) {
+        focusedWindow.send("logviewer.error", error.response.data);
+        return console.log(error);
     });
 }
 
 function getTotals(focusedWindow: WebContents) {
 
-    request(`${serverApiDomain}/totals`, { json: true }, (err, res, body) => {
-        if (err) {
-            focusedWindow.send("logviewer.error", err);
-            return console.log(err);
-        }
-
-        focusedWindow.send("logviewer.data-totals", body);
+    axios.get('/totals', {
+        baseURL: serverApiDomain,
+        responseType: "json"
+    })
+    .then(function (response) {
+        focusedWindow.send("logviewer.data-totals", response.data);
+    })
+    .catch(function (error) {
+        focusedWindow.send("logviewer.error", error.response.data);
+        return console.log(error);
     });
 }
 
 function getMessageTemplates(focusedWindow: WebContents) {
 
-    request(`${serverApiDomain}/messagetemplates`, { json: true }, (err, res, body) => {
-        if (err) {
-            focusedWindow.send("logviewer.error", err);
-            return console.log(err);
-        }
-
-        focusedWindow.send("logviewer.data-templates", body);
+    axios.get('/messagetemplates', {
+        baseURL: serverApiDomain,
+        responseType: "json"
+    })
+    .then(function (response) {
+        focusedWindow.send("logviewer.data-templates", response.data);
+    })
+    .catch(function (error) {
+        focusedWindow.send("logviewer.error", error.response.data);
+        return console.log(error);
     });
 }
 
 export function getLogs(focusedWindow: WebContents, pageNumber: number, filterExpression: string, sortOrder: SortOrder = SortOrder.Descending):void {
 
-    request(`${serverApiDomain}/search?pageNumber=${pageNumber}&filterExpression=${filterExpression}&sort=${sortOrder}`, { json: true }, (err, res, body) => {
-        if (err) {
-            focusedWindow.send("logviewer.error", err);
-            return console.log(err);
+    axios.get('/search', {
+        baseURL: serverApiDomain,
+        responseType: "json",
+        params: {
+            pageNumber: pageNumber,
+            filterExpression: filterExpression,
+            sort: sortOrder
         }
-
-        focusedWindow.send("logviewer.data-logs", body);
+    })
+    .then(function (response) {
+        focusedWindow.send("logviewer.data-logs", response.data);
+    })
+    .catch(function (error) {
+        focusedWindow.send("logviewer.error", error.response.data);
+        return console.log(error);
     });
 }
 
 export function exportFile(focusedWindow: WebContents, newFileName: string):void {
 
     // TODO: At some point in future specify the mesaage template in the UI of app
-    // So it can be passed to the export API endpoint
-    request(`${serverApiDomain}/export?newFileName=${newFileName}`, { json: true }, (err, res, body) => {
-        if (err) {
-            focusedWindow.send("logviewer.export-error", err);
-            return console.log(err);
-        }
+    // So it can be passed to the export API endpoint   
 
+    axios.get('/export', {
+        baseURL: serverApiDomain,
+        params: {
+            newFileName: newFileName
+        }
+    })
+    .then(function (response) {
         focusedWindow.send("logviewer.export-done");
+    })
+    .catch(function (error) {
+        focusedWindow.send("logviewer.export-error", error.response.data);
+        return console.log(error);
     });
 }
 
