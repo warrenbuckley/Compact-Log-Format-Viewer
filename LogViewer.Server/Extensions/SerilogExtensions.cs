e6a646e664ec01560bf2ce6f57e45982ca861308
Warren Buckley <warren@umbraco.com>
2022-02-07 12:30:21 +0000

    Add Has() function that is missing

---
+++ LogViewer.Server/Extensions/SerilogExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Serilog.Events;
+using System;
+
+namespace LogViewer.Server.Extensions
+{
+    public class SerilogExtensions
+    {
+        public static LogEventPropertyValue? Has(StringComparison comparison, LogEventPropertyValue? input)
+        {
+            // Comes in null if its not a property we have
+            // The input in the Has() is checking to see if that property exists
+            if(input != null)
+            {
+                // But if user does Has('SomeString')
+                // It would be a weird use, as its not checking a property existance
+                // but just some random string. It would still return as true though
+                return new ScalarValue(true);
+            }
+
+            return null;
+        }
+
+    }
+}
