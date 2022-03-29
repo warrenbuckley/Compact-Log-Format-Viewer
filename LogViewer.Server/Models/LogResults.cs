8900cc04dc377e86ecc773481aaf28f497e1e4c6
Warren Buckley <warren@umbraco.com>
2022-03-29 16:03:17 +0100

    New model that contains the paged results and message templates relative to the query

---
+++ LogViewer.Server/Models/LogResults.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace LogViewer.Server.Models
+{
+    public class LogResults
+    {
+        public PagedResult<LogMessage> Logs { get; set; }
+
+        public List<LogTemplate> MessageTemplates { get; set; }
+    }
+}
